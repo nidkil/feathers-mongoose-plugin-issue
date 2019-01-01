26227ee97ee41d816849ed1915989c77e0ed307d
nidkil <info@nidkil.com>
2019-01-01 16:39:45 +0100

    added options to connection to stop using deprecated methods and enable Mongoose debugging through config

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
+const mongooseOptions = {
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useNewUrlParser: true
+};
+
 module.exports = function (app) {
-  mongoose.connect(app.get('mongodb'), { useNewUrlParser: true });
+  const config = app.get('mongodb');
+  mongoose.connect(config.connection, mongooseOptions);
   mongoose.Promise = global.Promise;
+
+  mongoose.set('debug', config.debug);
 
   app.set('mongooseClient', mongoose);
 };
