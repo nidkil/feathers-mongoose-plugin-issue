0a37f9ca1e204105b1c8fdc86a6d9a1c0c091ed9
nidkil <info@nidkil.com>
2019-01-02 08:16:41 +0100

    changed Mongoose debug implementation

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const logger = require('./logger');
 
 const mongooseOptions = {
   useFindAndModify: false,
@@ -6,12 +7,38 @@ const mongooseOptions = {
   useNewUrlParser: true
 };
 
+function serializer(data, compact = true) {
+  if (compact) {
+    const query = JSON.stringify(data.query);
+    const doc = JSON.stringify(data.doc);
+    const options = JSON.stringify(data.options);
+    return `db.${data.coll}.${data.method}(${query}, ${doc}, ${options});`;
+  } else {
+    const query = JSON.stringify(data.query, null, '\t');
+    const doc = JSON.stringify(data.doc, null, '\t');
+    const options = JSON.stringify(data.options || {}, null, '\t');
+    return `db.${data.coll}.${data.method}(${query}, ${doc}, ${options});`;
+  }
+}
+
 module.exports = function (app) {
   const config = app.get('mongodb');
   mongoose.connect(config.connection, mongooseOptions);
   mongoose.Promise = global.Promise;
 
-  mongoose.set('debug', config.debug);
+  if (config.debug.enabled) {
+    const compact = config.debug.compact;
+    mongoose.set('debug', function (coll, method, query, doc, options) {
+      let data = {
+        coll,
+        method,
+        query,
+        doc,
+        options
+      };
+      logger.info('Mongoose: ' + serializer(data, compact) + (compact ? '\n' : ''));
+    });
+  }
 
   app.set('mongooseClient', mongoose);
 };
